fb21515b3716fc04626d895b98833c0cdfb60c1e
Oliver Pfeiffer <oliverpfeiffer@me.com>
2015-08-02 18:24:18 +0200

    First commit gulp-jade bp

---
+++ gulpfile.js
@@ -0,0 +1,170 @@
+
+// -----------v 02.08.2015---------------------------------------------
+// npm install   / npm update —save-dev
+// gulp bower    / bower-update
+// gulp
+// --------------------------------------------------------------------
+
+
+// --------------------------------------------------------------------
+// Plugins
+// --------------------------------------------------------------------
+
+var gulp        = require("gulp"),
+    bower       = require('gulp-bower'),
+    data        = require('gulp-data'),
+    browserSync = require('browser-sync'),
+    jade        = require("gulp-jade"),
+    minifyHTML  = require('gulp-minify-html'),
+    sass        = require("gulp-sass"),
+    autoprefix  = require("gulp-autoprefixer"),
+    minify_css  = require("gulp-minify-css"),
+    concat      = require("gulp-concat"),
+    rename      = require('gulp-rename'),
+    uglify      = require("gulp-uglify"),
+    jshint      = require("gulp-jshint"),
+    sourcemaps  = require("gulp-sourcemaps"),
+    notify      = require("gulp-notify"),
+    cache       = require('gulp-cache'),
+    size        = require('gulp-size'),
+    imagemin    = require("gulp-imagemin"),
+    pngquant    = require("imagemin-pngquant");
+
+//    connect     = require('gulp-connect'),
+//    plumber     = require("gulp-plumber"),
+
+
+// --------------------------------------------------------------------
+// Settings
+// --------------------------------------------------------------------
+
+var paths = {
+    'bower' : './assets/bower',
+    'assets': './assets',
+    'outputDev': './dev',
+    'outputDist': './dist'
+};
+
+
+// --------------------------------------------------------------------
+// Task: Bower
+// --------------------------------------------------------------------
+
+gulp.task('bower', function() { 
+    return bower()
+         .pipe(gulp.dest(paths.bower + '/')) ;
+});
+
+
+// --------------------------------------------------------------------
+// Task: Jade Templating
+// --------------------------------------------------------------------
+
+gulp.task('templates', function() {
+    return gulp.src(paths.assets + '/templates/**/*.jade')
+        .pipe(jade({
+            pretty: true
+        }))
+        .pipe(gulp.dest(paths.outputDev + '/'))
+        .pipe(browserSync.reload({stream:true}));
+});
+
+
+// --------------------------------------------------------------------
+// Task: Html
+// --------------------------------------------------------------------
+
+gulp.task('html', ['templates'], function() {
+  var opts = {
+    empty: true,
+    loose: true
+  };
+
+  return gulp.src(paths.outputDev + '/*.html')
+    .pipe(minifyHTML(opts))
+    .pipe(gulp.dest(paths.outputDist + '/'));
+});
+
+
+// --------------------------------------------------------------------
+// Task: Sass
+// --------------------------------------------------------------------
+
+gulp.task('sass', function() {
+  return gulp.src(paths.assets + '/scss/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      onError: browserSync.notify
+    }))
+    .pipe(autoprefix('last 2 version'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(paths.outputDev + '/css'))
+    .pipe(minify_css())
+    .pipe(gulp.dest(paths.outputDist + '/css'))
+    .pipe(size())
+    .pipe(browserSync.reload({stream:true}));
+});
+
+
+// --------------------------------------------------------------------
+// Task: Scripts
+// --------------------------------------------------------------------
+
+gulp.task('scripts', function() {
+  return gulp.src(paths.assets + '/scripts/**/*.js')
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest(paths.outputDev + '/js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.outputDist + '/js'))
+    .pipe(browserSync.reload({stream:true}));
+});
+
+
+// --------------------------------------------------------------------
+// Task: Images
+// --------------------------------------------------------------------
+
+gulp.task('images', function() {
+  return gulp.src(paths.assets + '/images/**/*')
+    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(gulp.dest(paths.outputDev + '/images'))
+    .pipe(gulp.dest(paths.outputDist + '/images'));
+});
+
+
+// --------------------------------------------------------------------
+// Task: Watch
+// --------------------------------------------------------------------
+
+gulp.task('watch', ['sass', 'browserSync'], function() {
+  // Watch jade files
+  gulp.watch(paths.assets + '/templates/**/*.jade', ['templates']);
+  // Watch html files
+  gulp.watch(paths.outputDev + '/*.html', ['html'], browserSync.reload);
+  // Watch .scss files
+  gulp.watch(paths.assets + '/scss/**/*.scss', ['sass']);
+  // Watch .js files
+  gulp.watch(paths.assets + '/scripts/**/*.js', ['scripts']);
+  // Watch image files
+  gulp.watch(paths.assets + '/images/**/*', ['images']);
+});
+
+
+// --------------------------------------------------------------------
+// Task: Browser-Sync
+// --------------------------------------------------------------------
+
+gulp.task('browserSync', ['sass'], function() {
+    browserSync({
+        server: {
+            baseDir: 'dev'
+        }
+    });
+});
+
+
+// --------------------------------------------------------------------
+// Task: Default
+// --------------------------------------------------------------------
+
+gulp.task('default', ['sass', 'scripts', 'images', 'html', 'browserSync', 'watch']);
